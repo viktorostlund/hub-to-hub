250b6fc60fda47e96f119efdc63cb7e4c3ed0d8b
viktorostlund <viktorostlund@hotmail.com>
2020-05-10 19:37:09 +0200

    refactor cards and card components

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Cards.css';
-import Card from './Card';
+// import Card from './Card';
 
 function Cards({users, deleteUser}) {
   return (
     <div className="Cards">
-        {users.map(user => <Card user={user} deleteUser={deleteUser}/>)}
+        {users.map(user =>
+        <div>
+          <p className="Card"><b>{user.name}</b></p>
+          {/* <p>{user.created_at}</p> */}
+          <p className="Card">Followers: {user.Followers}</p>
+          <p className="Card">Repos: {user.Repos}</p>
+          <p className="Card"><button type="submit" onClick={() => deleteUser(user.name)}>X</button></p>
+        </div>
+      )}
     </div>
   );
 }
